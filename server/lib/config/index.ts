baaa82a75ed14ed07f3cd19cfb23876874a1fc5b
Olivier <devangularjs@gmail.com>
2018-02-25 01:29:51 +0100

    refactor index.ts
    fixes #13

--- server/lib/config/index.ts
+++ server/lib/config/index.ts
@@ -8,123 +8,168 @@ import { exportDevProperties } from './env/development.properties';
 import { exportProdProperties } from './env/production.properties';
 import { exportAssets } from './assets/default.assets';
 
-let env;
-let propertiesFile;
+class PropertiesConfiguration {
+  public env;
+  public propertiesFile;
 
-const getGlobbedPaths = (globPatterns, excludes?) => {
-  // Définition du regex de recherche
-  const urlRegex = new RegExp('^(?:[a-z]+:)?//', 'i');
+  constructor() { }
 
-  // Initialisation de la variable de retour
-  let output = [];
+  /**
+   * Parcours des noeuds et des arrays des assets et renvoi la liste des fichiers des répertoires
+   * @method getGlobbedPaths
+   * @private
+   * @param {*} globPatterns
+   * @param {*} [excludes]
+   * @returns {array}
+   * @memberof PropertiesConfiguration
+   */
+  private getGlobbedPaths(globPatterns: any, excludes?: any): any {
+    // Définition du regex de recherche
+    const urlRegex = new RegExp('^(?:[a-z]+:)?//', 'i');
 
-  // Récupération des fichiers
-  // globPatterns => permet de déclarer les assets
-  // Si le paramètre globPatterns[] => itération sur l'array
-  if (_.isArray(globPatterns)) {
-    globPatterns.forEach(globPattern => {
-      // incrémentation de l'array output de retour
-      output = _.union(output, getGlobbedPaths(globPattern, excludes));
-    });
-    // si le paramètre globPatterns est un string alors il faut pousser la valeur dans l'array output
-  } else if (_.isString(globPatterns)) {
-    if (urlRegex.test(globPatterns)) {
-      output.push(globPatterns);
-    } else {
-      // déclaration de la variable files tel que files prend la valeur du nom du fichier si cela correspond au pattern
-      let files = glob.sync(globPatterns);
-      if (excludes) {
-        files = files.map(file => {
-          if (_.isArray(excludes)) {
-            let i;
-            for (i in excludes) {
-              if (excludes.hasOwnProperty(i)) {
-                file = file.replace(excludes[i], '');
+    // Initialisation de la variable de retour
+    let output = [];
+
+    // Récupération des fichiers
+    // globPatterns => permet de déclarer les assets
+    // Si le paramètre globPatterns[] => itération sur l'array
+    if (_.isArray(globPatterns)) {
+      globPatterns.forEach(globPattern => {
+        // incrémentation de l'array output de retour
+        output = _.union(output, this.getGlobbedPaths(globPattern, excludes));
+      });
+      // si le paramètre globPatterns est un string alors il faut pousser la valeur dans l'array output
+    } else if (_.isString(globPatterns)) {
+      if (urlRegex.test(globPatterns)) {
+        output.push(globPatterns);
+      } else {
+        // déclaration de la variable files tel que files prend la valeur du nom du fichier si cela correspond au pattern
+        let files = glob.sync(globPatterns);
+        if (excludes) {
+          files = files.map(file => {
+            if (_.isArray(excludes)) {
+              let i;
+              for (i in excludes) {
+                if (excludes.hasOwnProperty(i)) {
+                  file = file.replace(excludes[i], '');
+                }
               }
+            } else {
+              file = file.replace(excludes, '');
             }
-          } else {
-            file = file.replace(excludes, '');
-          }
-          return file;
-        });
+            return file;
+          });
+        }
+        // Passage de l'ensemble des fichiers trouvés à la valeur output
+        output = _.union(output, files);
       }
-      // Passage de l'ensemble des fichiers trouvés à la valeur output
-      output = _.union(output, files);
     }
-  }
-  return output;
-
-};
-
-
-const initGlobalPropFiles: any = (initProperties, assets) => {
-  initProperties.files = {
-    server: {},
-    client: {}
-  };
-
-  // Récupération du fichier gulp
-  initProperties.files.server.gulp = getGlobbedPaths(assets.server.gulpConfig);
-
-  // Récupération allTS
-  initProperties.files.server.allTS = getGlobbedPaths(assets.server.allTS);
-
-  // Récupération config
-  initProperties.files.server.config = getGlobbedPaths(assets.server.config);
-
-  // Récupération models
-  initProperties.files.server.models = getGlobbedPaths(assets.server.models);
-
-  // Récupération routes
-  initProperties.files.server.routes = getGlobbedPaths(assets.server.routes);
-
-};
-
-
-const validateEnvironementDefinition: any = () => {
-  if (!process.env.NODE_ENV) {
-    console.warn(`ALerte: Environnement d'exécution non définie`);
-    console.warn(`Application par défaut de l'environnement de développement`);
-    process.env.NODE_ENV = 'development';
-  }
-  env = process.env.NODE_ENV;
-
-  propertiesFile = choosePropertiesFile(env);
-  };
-
-const choosePropertiesFile: any = (envType) => {
-
-  if (envType === 'development') {
-    return exportDevProperties;
-  } else if (envType === 'production') {
-    return exportProdProperties;
-  } else {
-    return exportDevProperties;
+    return output;
   }
 
-};
+
+  /**
+   * Récupération des fichiers ts et associations dans un noeud spécifique du fichier properties
+   * @method initGlobalPropertiesFiles
+   * @private
+   * @param {object} initProperties
+   * @param {object} assets
+   * @memberof PropertiesConfiguration
+   */
+  private initGlobalPropertiesFiles(initProperties: any, assets: any): any {
+    // Initialisation du noeud de l'object
+    initProperties.files = {
+      server: {},
+      client: {}
+    };
+
+    // Récupération du fichier GULP
+    initProperties.files.server.gulp = this.getGlobbedPaths(assets.server.gulpConfig);
+
+    // Récupération des fichiers allTS
+    initProperties.files.server.allTS = this.getGlobbedPaths(assets.server.allTS);
+
+    // Récupération des fichiers de config des modules
+    initProperties.files.server.config = this.getGlobbedPaths(assets.server.config);
+
+    // Récupération des fichiers de models des modules
+    initProperties.files.server.models = this.getGlobbedPaths(assets.server.models);
+
+    // Récupération des routes des modules
+    initProperties.files.server.routes = this.getGlobbedPaths(assets.server.routes);
+
+  }
 
 
-const initGlobalProperties: any = () => {
+  /**
+   * Choix du fichier de properties selon l'environnement
+   * @method selectPropertiesFileByEnv
+   * @private
+   * @param {string} envType
+   * @returns {object} properties dépendant de l'environnement
+   * @memberof PropertiesConfiguration
+   */
+  private selectPropertiesFileByEnv(envType: string): any {
 
-  // Vérification de la variable d'environnement
-  validateEnvironementDefinition();
+    if (envType === 'development') {
+      return exportDevProperties;
+    } else if (envType === 'production') {
+      return exportProdProperties;
+    } else {
+      return exportDevProperties;
+    }
 
-  // Merge des properties
-  const initProperties = _.merge(exportDefaultProperties, propertiesFile);
+  }
 
-  // Enrichissement avec les informations du package.json
-  const pkg = require('../../../package.json');
-  initProperties.app.version = pkg.version;
 
-  // Récupération des assets
-  const assets = exportAssets;
+  /**
+   * Validation que l'environnement est bien configuré
+   * @method validateEnvironmentDefinition
+   * @private
+   * @memberof PropertiesConfiguration
+   */
+  private validateEnvironmentDefinition(): any {
+    if (!process.env.NODE_ENV) {
+      console.warn(`ALerte: Environnement d'exécution non définie`);
+      console.warn(`Application par défaut de l'environnement de développement`);
+      process.env.NODE_ENV = 'development';
+    }
+    this.env = process.env.NODE_ENV;
 
-  // Initialisation des fichiers
-  initGlobalPropFiles(initProperties, assets);
+    this.propertiesFile = this.selectPropertiesFileByEnv(this.env);
+  }
 
-  return initProperties;
 
-};
+  /**
+   * Initialisation de l'objet properties
+   * Contient l'ensemble des paramètres de l'application
+   * @method initGlobalProperties
+   * @returns {*} initProperties
+   * @memberof PropertiesConfiguration
+   */
+  public initGlobalProperties(): any {
 
-export const properties = initGlobalProperties();
+    // Vérification de la variables d'environnement
+    this.validateEnvironmentDefinition();
+
+    // Merge des fichiers de properties local + environnement
+    // les properties de environnement écrasent celles de local
+    const initProperties = _.merge(exportDefaultProperties, this.propertiesFile);
+
+    // Enrichissement des properties avec les informations du package.json
+    const pkg = require('../../../package.json');
+    initProperties.app.version = pkg.version;
+
+    // Récupération des assets
+    const assets = exportAssets;
+
+    // Initialisation des assets
+    this.initGlobalPropertiesFiles(initProperties, assets);
+
+    return initProperties;
+
+  }
+
+}
+
+export const properties = new PropertiesConfiguration().initGlobalProperties();
