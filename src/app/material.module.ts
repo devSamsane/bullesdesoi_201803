31f1cc36add616cb0e182ceddf73494d12355058
Olivier <devangularjs@gmail.com>
2018-02-26 02:19:01 +0100

    infra(material): init module material angular

---
+++ src/app/material.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import {
+  MatToolbarModule,
+  MatSidenavModule
+} from '@angular/material';
+
+@NgModule({
+  imports: [],
+  exports: []
+})
+export class MaterialModule { }
