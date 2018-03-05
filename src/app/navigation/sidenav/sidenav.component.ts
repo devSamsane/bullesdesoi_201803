46993f700e06cfe35a4f630f5c1bd24c5610115f
devSamsane <>
2018-03-05 22:24:06 +0100

    feat(home): structure de la page

--- src/app/navigation/sidenav/sidenav.component.ts
+++ src/app/navigation/sidenav/sidenav.component.ts
@@ -1,11 +1,12 @@
-import { Component, OnInit, NgZone, ViewChild } from '@angular/core';
-import { ActivatedRoute, Params, Router, RouterModule } from '@angular/router';
+import { Component, OnInit, NgZone } from '@angular/core';
+import { ActivatedRoute, UrlSegment, Params, Router, RouterModule, NavigationEnd } from '@angular/router';
 
 import { Observable } from 'rxjs/Observable';
 import { combineLatest } from 'rxjs/observable/combineLatest';
 
  import { NavigationItems } from './../../shared/nav-items/nav-items';
 import { MatSidenav } from '@angular/material';
+import { Subscription } from 'rxjs/Subscription';
 
 
 @Component({
@@ -16,18 +17,46 @@ import { MatSidenav } from '@angular/material';
 export class SidenavComponent implements OnInit {
 
   params: Observable<Params>;
+  scrollSpy: number;
+  private scrollTimeout = 50;
+  private currentUrl: string;
+  private activeUrlSubscription$: Subscription;
 
   constructor(
     public navItems: NavigationItems,
-    private _route: ActivatedRoute,
-    private _router: Router,
-    zone: NgZone
+    private activeRoute: ActivatedRoute,
+    private router: Router,
+    // zone: NgZone
   ) { }
+
+  // scrollSpyHandler(event) {
+  //   setTimeout(() => {
+  //     if (this.currentUrl === '/') {
+  //       this.scrollSpy = event.target.scrollTop;
+  //       console.log(this.scrollSpy);
+  //     } else {
+  //       console.log('pas sur home');
+  //     }
+  //   }, this.scrollTimeout);
+  // }
 
   ngOnInit() {
     this.params = combineLatest(
-      this._route.pathFromRoot.map(route => route.params), Object.assign
+      this.activeRoute.pathFromRoot.map(route => route.params), Object.assign
     );
   }
+
+  // ngAfterViewInit() {
+  //   this.activeUrlSubscription$ = this.router.events
+  //     .subscribe((res) => {
+  //       this.currentUrl = this.router.url;
+  //   });
+  // }
+
+  // ngOnDestroy() {
+  //   if (this.activeUrlSubscription$) {
+  //     this.activeUrlSubscription$.unsubscribe();
+  //   }
+  // }
 
 }
