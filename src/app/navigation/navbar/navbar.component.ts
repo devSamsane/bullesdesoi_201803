ab6128cdc7950bc768d9e951fe4e65d9cf05d405
devSamsane <>
2018-03-10 08:16:13 +0100

    feat(navbar): changement d'état de la navbar selon le path et le scroll

--- src/app/navigation/navbar/navbar.component.ts
+++ src/app/navigation/navbar/navbar.component.ts
@@ -1,25 +1,75 @@
-import { Component, OnInit, Output, EventEmitter, Input } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  Output,
+  EventEmitter,
+  Input,
+  ElementRef,
+  Inject,
+  OnDestroy
+} from '@angular/core';
+import { DOCUMENT } from '@angular/platform-browser';
+import { trigger, state, style, animate, transition } from '@angular/animations';
+
+import {debounceTime} from 'rxjs/operators/debounceTime';
+import {takeUntil} from 'rxjs/operators/takeUntil';
+import {fromEvent} from 'rxjs/observable/fromEvent';
+
 import { ComponentPageTitle } from '../../shared/page-title/page-title';
+import { Subject } from 'rxjs/Subject';
+import { ActivatedRoute, Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'bds-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  styleUrls: ['./navbar.component.scss'],
+  animations: [
+    // routerTransition,
+    trigger('navbarStateToggle', [
+      state('transparent', style({
+        backgroundColor: 'rgba(126, 87, 194, 0)'
+      })),
+      state('opaque', style({
+        backgroundColor: 'rgba(126, 87, 194, 1)',
+      })),
+      transition('transparent => opaque', animate('500ms ease-in')),
+      transition('opaque => transparent', animate('500ms ease-out'))
+    ])
+  ]
 })
-export class NavbarComponent implements OnInit {
+export class NavbarComponent implements OnInit, OnDestroy {
 
+  @Input() container: string;
   @Output() isSidenavToggle = new EventEmitter<void>();
+
+  public navbarStateToggle = 'transparent';
   title = 'Bulles de Soi';
   toggleIcon: boolean;
+  private destroyed = new Subject();
+  private scrollContainer: any;
+  private SCROLL_TOOLBAR_CHANGE_STATE = 200;
+  private scrollOffset: any = 0;
 
-  constructor(public componentPageTitle: ComponentPageTitle) { }
+  _rootUrl: string;
+
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    public componentPageTitle: ComponentPageTitle,
+    @Inject(DOCUMENT) private document: Document
+  ) {
+    this.router.events.pipe(takeUntil(this.destroyed)).subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        const rootUrl = router.url.split('#')[0];
+        this._rootUrl = rootUrl;
+        this.setNavbarState(rootUrl);
+      }
+    });
+  }
+
 
   getTitle() {
     return this.componentPageTitle.title;
-  }
-
-  ngOnInit() {
-    this.toggleIcon = false;
   }
 
   toggleSidenav() {
@@ -28,6 +78,51 @@ export class NavbarComponent implements OnInit {
 
   toggleIconClick() {
     this.toggleIcon = !this.toggleIcon;
+  }
+
+  private setNavbarState(url: string) {
+    if (url !== '/') {
+      this.navbarStateToggle = 'opaque';
+    } else {
+      this.navbarStateToggle = 'transparent';
+    }
+  }
+
+  private getNavbarState(scrollValue: number) {
+    if (scrollValue > this.SCROLL_TOOLBAR_CHANGE_STATE) {
+      this.navbarStateToggle = 'opaque';
+    } else if (scrollValue <= this.SCROLL_TOOLBAR_CHANGE_STATE) {
+      this.navbarStateToggle = 'transparent';
+    }
+  }
+
+  private getScrollOffset(scrollValue: any): void {
+    if (scrollValue !== 'undefined') {
+      this.getNavbarState(scrollValue);
+    } else {
+      this.navbarStateToggle = 'transparent';
+    }
+  }
+
+  ngOnInit() {
+    this.toggleIcon = false;
+
+    Promise.resolve().then(() => {
+      this.scrollContainer = this.container ? this.document.querySelector(this.container)['mat-sidenav-content'] : window;
+      fromEvent(this.scrollContainer, 'scroll').pipe(
+        takeUntil(this.destroyed),
+        debounceTime(10))
+        .subscribe((event) => {
+          this.scrollOffset = this.scrollContainer.window.pageYOffset;
+          if (this._rootUrl === '/') {
+            this.getScrollOffset(this.scrollOffset);
+          }
+        });
+    });
+  }
+
+  ngOnDestroy() {
+    this.destroyed.unsubscribe();
   }
 
 }
--- src/app/navigation/sidenav/sidenav.component.ts
+++ src/app/navigation/sidenav/sidenav.component.ts
@@ -1,12 +1,11 @@
-import { Component, OnInit, NgZone } from '@angular/core';
-import { ActivatedRoute, UrlSegment, Params, Router, RouterModule, NavigationEnd } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params, Router, RouterModule } from '@angular/router';
 
 import { Observable } from 'rxjs/Observable';
 import { combineLatest } from 'rxjs/observable/combineLatest';
-
- import { NavigationItems } from './../../shared/nav-items/nav-items';
-import { MatSidenav } from '@angular/material';
 import { Subscription } from 'rxjs/Subscription';
+
+import { NavigationItems } from './../../shared/nav-items/nav-items';
 
 
 @Component({
@@ -17,28 +16,11 @@ import { Subscription } from 'rxjs/Subscription';
 export class SidenavComponent implements OnInit {
 
   params: Observable<Params>;
-  scrollSpy: number;
-  private scrollTimeout = 50;
-  private currentUrl: string;
-  private activeUrlSubscription$: Subscription;
 
   constructor(
     public navItems: NavigationItems,
     private activeRoute: ActivatedRoute,
-    private router: Router,
-    // zone: NgZone
   ) { }
-
-  // scrollSpyHandler(event) {
-  //   setTimeout(() => {
-  //     if (this.currentUrl === '/') {
-  //       this.scrollSpy = event.target.scrollTop;
-  //       console.log(this.scrollSpy);
-  //     } else {
-  //       console.log('pas sur home');
-  //     }
-  //   }, this.scrollTimeout);
-  // }
 
   ngOnInit() {
     this.params = combineLatest(
@@ -46,17 +28,5 @@ export class SidenavComponent implements OnInit {
     );
   }
 
-  // ngAfterViewInit() {
-  //   this.activeUrlSubscription$ = this.router.events
-  //     .subscribe((res) => {
-  //       this.currentUrl = this.router.url;
-  //   });
-  // }
-
-  // ngOnDestroy() {
-  //   if (this.activeUrlSubscription$) {
-  //     this.activeUrlSubscription$.unsubscribe();
-  //   }
-  // }
 
 }
